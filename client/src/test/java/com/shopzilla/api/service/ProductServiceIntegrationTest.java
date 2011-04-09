4edcfb47ec5f90c30c862b54ea888808140a6744
Andrew Look <alook@shopzilla.com>
2011-04-08 17:23:38 -0700

    Added in copyright headers corresponding to the Apache v2 License

--- client/src/test/java/com/shopzilla/api/service/ProductServiceIntegrationTest.java
+++ client/src/test/java/com/shopzilla/api/service/ProductServiceIntegrationTest.java
@@ -1,13 +1,21 @@
 /**
- * Copyright (C) 2004 - 2011 Shopzilla, Inc. 
- * All rights reserved. Unauthorized disclosure or distribution is prohibited.
+ * Copyright 2011 Shopzilla.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.shopzilla.api.service;
 
 import com.shopzilla.services.catalog.ProductResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +25,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
